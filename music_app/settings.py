80c5d4a32a3c29f6d70ce1e2965fd05ae82efbd9
Dieuveille BOUSSA ELLENGA <d.boussaellenga@h3hitema.fr>
2021-10-10 01:25:16 +0200

    created a new app: account and implemented the model for the music app

--- music_app/settings.py
+++ music_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'music',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# LOGIN_REDIRECT_URL = '/music/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+
