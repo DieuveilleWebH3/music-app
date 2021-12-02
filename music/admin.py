09fc9a75f1da3254c2e591e16ae5b37bbbf7ab59
Dieuveille BOUSSA ELLENGA <d.boussaellenga@h3hitema.fr>
2021-12-02 16:04:20 +0100

    finished music models, for now, working on views now, ...

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    list_filter = ("title", )
+    search_fields = ("title",)
+
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "artist", "genres", "is_favorites", "user")
+    list_filter = ("genre", "artist")
+    search_fields = ("title", "artist", "genre")
+
+    prepopulated_fields = {'slug': ('title',)}
+
+    def genres(self, obj):
+        return "\n".join([genre.title + ', ' for genre in obj.is_favorite.all()])
+
+    def is_favorites(self, obj):
+        return "\n".join([user.username + ', ' for user in obj.is_favorite.all()])
+
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "album", "is_favorites")
+    list_filter = ("album",)
+    search_fields = ("title",)
+
+    prepopulated_fields = {'slug': ('title',)}
+
+    def is_favorites(self, obj):
+        return "\n".join([user.username + ', ' for user in obj.is_favorite.all()])
+
+
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "user", "songs")
+    list_filter = ("user",)
+    search_fields = ("title",)
+
+    prepopulated_fields = {'slug': ('title',)}
+
+    def songs(self, obj):
+        return "\n".join([song.title + ', ' for song in obj.is_favorite.all()])
+
+
+admin.site.register(Song, SongAdmin)
+
+admin.site.register(Album, AlbumAdmin)
+
+admin.site.register(Genre, GenreAdmin)
+
+admin.site.register(Playlist, PlaylistAdmin)
+
--- music/models.py
+++ music/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from account.models import *
+from django.contrib.auth.models import Permission
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Album(models.Model):
+
+    def get_upload_path(instance, filename):
+        return 'artist/{0}/{1}'.format(instance.artist, filename)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    artist = models.CharField(max_length=250)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    # genre = models.CharField(max_length=100)
+    genre = models.ManyToManyField(Genre, blank=True, related_name='genres')
+    album_logo = models.ImageField(upload_to=get_upload_path, blank=True)
+    # is_favorite = models.BooleanField(default=False)
+    is_favorite = models.ManyToManyField(User, blank=True, related_name='favorite_albums')
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title + ' - ' + self.artist
+
+
+class Song(models.Model):
+
+    def get_upload_path(instance, filename):
+        return 'artist/{0}/{1}'.format(instance.album.artist, filename)
+
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    audio_file = models.FileField(upload_to=get_upload_path, null=False, blank=False)
+    is_favorite = models.ManyToManyField(User, blank=True, related_name='favorite_songs')
+    # is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    song = models.ManyToManyField(Song, blank=True, related_name='playlist_songs')
+    # song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
